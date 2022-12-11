c58ce916b552f6d7a15271fed901e1d2f0fc47c5
Zachery Jensen <ztjio@icloud.com>
2022-12-10 17:33:34 -0700

    Day 8: Part 1 working

---
+++ src/bin/day08.rs
@@ -0,0 +1,131 @@
+use std::fmt::{Display, Formatter};
+
+use aoc::Parser;
+
+struct Grid {
+    width: usize,
+    height: usize,
+    data: Vec<u8>,
+}
+
+/// Don't try to read from this before you've added at least one row, and don't add rows with differing lengths
+/// otherwise you can expect panics to occur.
+impl Grid {
+    fn new() -> Self {
+        Self {
+            width: 0,
+            height: 0,
+            data: Vec::new(),
+        }
+    }
+
+    fn add_row(&mut self, row: &[u8]) {
+        if self.width == 0 {
+            self.width = row.len();
+        } else if self.width != row.len() {
+            panic!("Row length does not match grid width, you can't change the row length after the first row is added");
+        }
+        self.height += 1;
+        self.data.extend_from_slice(row);
+    }
+
+    fn height(&self) -> usize {
+        self.height
+    }
+
+    fn width(&self) -> usize {
+        if self.width > 0 {
+            self.width
+        } else {
+            panic!("Grid has no rows")
+        }
+    }
+
+    fn get(&self, x: usize, y: usize) -> u8 {
+        self.data[y * self.width + x]
+    }
+
+    fn visible_from_outside(&self) -> u64 {
+        let mut count = 0;
+        for y in 0..self.height {
+            for x in 0..self.width {
+                // Border
+                if x == 0 || y == 0 || x == self.width - 1 || y == self.height - 1 {
+                    count += 1;
+                    continue;
+                }
+                let height = self.get(x, y);
+                let mut west_blocked = false;
+                let mut east_blocked = false;
+                let mut north_blocked = false;
+                let mut south_blocked = false;
+                for west in 0..x {
+                    if self.get(west, y) >= height {
+                        west_blocked = true;
+                        break;
+                    }
+                }
+                for east in x + 1..self.width {
+                    if self.get(east, y) >= height {
+                        east_blocked = true;
+                        break;
+                    }
+                }
+                for north in 0..y {
+                    if self.get(x, north) >= height {
+                        north_blocked = true;
+                        break;
+                    }
+                }
+                for south in y + 1..self.height {
+                    if self.get(x, south) >= height {
+                        south_blocked = true;
+                        break;
+                    }
+                }
+                if !west_blocked || !east_blocked || !north_blocked || !south_blocked {
+                    count += 1;
+                }
+            }
+        }
+        count
+    }
+}
+
+/// Good enough for AOC, but, could be more efficient
+impl Display for Grid {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        for y in 0..self.height {
+            for x in 0..self.width {
+                write!(f, "{}", self.get(x, y))?;
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
+// I wrote this one before I decided to split up part 1 & 2 executions
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = aoc::Args::parse();
+    let path = if cli.real {
+        "input/08/real.txt"
+    } else {
+        "input/08/example.txt"
+    };
+
+    let input = std::fs::read_to_string(path)?;
+    let mut grid = Grid::new();
+    input
+        .lines()
+        .map(|line| {
+            line.trim()
+                .chars()
+                .map(|c| (c as u8) - 48)
+                .collect::<Vec<u8>>()
+        })
+        .for_each(|row| grid.add_row(&row));
+    //println!("{}", grid);
+    println!("Visible from outside: {}", grid.visible_from_outside());
+    Ok(())
+}
